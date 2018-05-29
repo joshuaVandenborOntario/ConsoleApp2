ff5e1b2a8fa3c69025c491f4d9b3b2f962808ec8
MOH\VandenJo <joshua.vandenbor@ontario.ca>
2018-05-29 12:34:58 -0400

    added a bad code smell to test sonarqube

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,11 +10,17 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            int testvariable = 0
             Console.WriteLine("Hello World");
 
             if(1 == 2)
             {
                 Console.WriteLine("This is impossible");
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                testvariable = +1;
             }
         }
 
