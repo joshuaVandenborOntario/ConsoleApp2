34ade7da48773e7b87b105f9c73426b35b1da728
MOH\VandenJo <joshua.vandenbor@ontario.ca>
2018-06-04 15:45:10 -0400

    made all tests pass

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -20,7 +20,7 @@ namespace UnitTestProject1
         {
             ConsoleApp2.Program app = new ConsoleApp2.Program();
             String message = app.returnString();
-            Assert.AreEqual("Hello", message);
+            Assert.AreEqual("Hello world", message);
         }
 
         [TestCategory("Nightly"), TestMethod()]
